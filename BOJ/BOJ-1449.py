29a13a7bd6d1b29a37a8a7fac626d59bf0e663d7
이현진 <princess7x7@naver.com>
2022-08-03 15:05:28 +0900

    # 2022/08/03 # 정렬

---
+++ BOJ/BOJ-1449.py
@@ -0,0 +1,18 @@
+# BOJ 1449
+import sys
+input = sys.stdin.readline
+
+N, L = map(int, input().split())
+leak = list(map(int, input().split()))
+leak.sort()
+start = leak[0] - 0.5
+count = 1
+
+for t in leak[1:]:
+    if start+L >= t + 0.5:
+        continue
+    else:
+        start = t - 0.5
+        count += 1
+
+print(count)
